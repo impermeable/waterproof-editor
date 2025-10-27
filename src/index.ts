51f0729554b84dfe6c05973690b2b31ce5e82965
DikieDick <26772815+DikieDick@users.noreply.github.com>
2025-10-27 17:59:31 +0100

    Export default tag serializer

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+
 // Export the Editor class
 export { WaterproofEditor } from "./editor";
 export { WaterproofSchema } from "./schema";
@@ -5,3 +6,4 @@ export * from "./document";
 export * from "./api";
 export { defaultToMarkdown } from "./translation";
 export * as "markdown" from "./markdown-defaults";
+export { DefaultTagSerializer } from "./serialization/DocumentSerializer";
