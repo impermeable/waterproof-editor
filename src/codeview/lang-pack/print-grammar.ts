b127112db2f55e378fc35e4a459ce14832bc5f63
Jim Portegies <j.w.portegies@tue.nl>
2025-10-29 07:43:19 +0100

    Allow for multiple arguments at various places

--- src/codeview/lang-pack/print-grammar.ts
+++ src/codeview/lang-pack/print-grammar.ts
@@ -2,13 +2,12 @@ import { parser } from "./syntax";
 
 // const source = `Obtain such m.`;
 const source = `Example example1_1_1 (a b c : ℤ) :
-  c | b ⇒ b | a ⇒ c | a.
+  c | b ⇒ b | a ⇒ True ⇒ c | a.
 Proof.
-Assume that c | b as (i).
-Assume that b | a as (ii).
+Assume that c | b as (i), b | a as (ii) and True as (iii).
 By (i) it holds that ∃ n ∈ ℤ, b = n * c.
 Obtain such n.
-By (ii) it holds that ∃ m ∈ ℤ, a = m * b.
+By (i) and (ii) it holds that ∃ m ∈ ℤ, a = m * b.
 Obtain such m.
 It holds that c * (n * m) = a.
 It suffices to show that ∃ k ∈ ℤ, a = k * c.
