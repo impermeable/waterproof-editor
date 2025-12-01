dbfa327f937255b83d892233dac757fec525908a
DikieDick <26772815+DikieDick@users.noreply.github.com>
2025-12-01 13:12:02 +0100

    Add example unit test

---
+++ __tests__/diagnostics.test.ts
@@ -0,0 +1,49 @@
+/**
+ * @jest-environment jsdom
+ */
+import { expect } from "@jest/globals";
+
+import { WaterproofEditor } from "../src/editor";
+import { DocChange, StringCell, ThemeStyle, WaterproofEditorConfig, WaterproofMapping } from "../src/api";
+
+class idMapping extends WaterproofMapping {
+    getMapping = () => new Map<number, StringCell>();
+    get version() { return -1; }
+    findPosition = (idx: number) => idx;
+    findInvPosition = (idx: number) => idx;
+    update = () => {
+        const ch: DocChange = {
+            endInFile: -1,
+            startInFile: -1,
+            finalText: ""
+        };
+        return ch;
+    }
+}
+
+const cfg: WaterproofEditorConfig = {
+    api: {
+        applyStepError: () => {},
+        cursorChange: () => {},
+        documentChange: () => {},
+        editorReady: () => {},
+        executeCommand: () => {},
+        executeHelp: () => {},
+        lineNumbers: () => {},
+        viewportHint: () => {},
+    },
+    completions: [],
+    documentConstructor: () => [],
+    mapping: idMapping,
+    symbols: []
+}
+
+
+test("test", () => {
+    const el = new HTMLDivElement();
+    const _editor = new WaterproofEditor(el, cfg, ThemeStyle.Light);
+
+      
+
+    expect(true).toBe(true);
+});
