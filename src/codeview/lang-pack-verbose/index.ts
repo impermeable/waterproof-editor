4d77dbfc9c04d947259a03452e693d6f2b919028
Oliwia Hejduk <o.hejduk@student.tue.nl>
2026-01-14 10:43:02 +0000

    Verbose tactics highlighting

--- src/codeview/lang-pack-verbose/index.ts
+++ src/codeview/lang-pack-verbose/index.ts
@@ -11,26 +11,32 @@ import { parser } from "./syntax"
 // Defining custom tags for specific elements of the Lean Verbose
 const tags = {
     verbose: Tag.define(),
+    tactic: Tag.define(),
     argument: Tag.define(),
     param: Tag.define(),
     string: Tag.define(),
     comment: Tag.define(),
+    bracket: Tag.define(),
 }
 
 // Highlighting specific elements of the Lean Verbose
 export const highlight_dark = HighlightStyle.define([
-    { tag: tags.verbose, color: "#912828" },   
+    { tag: tags.verbose, color: "#912828" },
+    { tag: tags.tactic, color: "#56b3ffff" },
     { tag: tags.param, color: "#0077ee" },
-    { tag: tags.string, colors: "#00aa00" },
-    { tag: tags.comment, colors: "#665f5fff" },
- ])
+    { tag: tags.string, color: "#00aa00" },
+    { tag: tags.comment, color: "#9ea0b1ff" },
+    { tag: tags.bracket, color: "#ff0000" },
+])
 
 // Highlighting specific elements of the Lean Verbose
 export const highlight_light = HighlightStyle.define([
-    { tag: tags.verbose, color: "#eb0808ff" },   
+    { tag: tags.verbose, color: "#eb0808ff" },
+    { tag: tags.tactic, color: "#004cf0ff" },
     { tag: tags.param, color: "#0077aa" },
-    { tag: tags.string, colors: "#00aa00" },
-    { tag: tags.comment, colors: "#3d3b3bff" },
+    { tag: tags.string, color: "#00aa00" },
+    { tag: tags.comment, color: "#787c99" },
+    { tag: tags.bracket, color: "#ff0000" },
 ]);
 
 // Defining the Lean Verbose syntax, highlighting and indentation
@@ -38,13 +44,49 @@ export const leanVerboseLanguage = LRLanguage.define({
     parser: parser.configure({
         props: [
             styleTags({
-                "Verbose": tags.verbose,
-                // "Argument": tags.argument,
-                "ProofBegin": tags.verbose,
-                "Param": tags.param,
+                // structure
+                "Example": tags.verbose,
+                "Exercise": tags.verbose,
+                "Proof": tags.verbose,
+                "QED": tags.verbose,
+                
+                "Given": tags.param,
+                "Assume": tags.param,
+                "Conclusion": tags.param,
+
+                // tactics / connectors / english keywords
+                "Fix": tags.tactic,
+                "By": tags.tactic,
+                "Lets": tags.tactic,
+                "Let": tags.tactic,
+                "Us": tags.tactic,
+                "Prove": tags.tactic,
+                "That": tags.tactic,
+                "We": tags.tactic,
+                "Compute": tags.tactic,
+                "Conclude": tags.tactic,
+                "ByWord": tags.tactic,
+                "Applied": tags.tactic,
+                "To": tags.tactic,
+                "Using": tags.tactic,
+                "Get": tags.tactic,
+                "Such": tags.tactic,
+                "It": tags.tactic,
+                "Suffices": tags.tactic,
+
+                // generic
+                "Chunk": tags.argument,
                 "Comment": tags.comment,
                 "String": tags.string,
-            })
+
+                // punctuation
+                "LParen": tags.bracket,
+                "RParen": tags.bracket,
+                "LBrace": tags.bracket,
+                "RBrace": tags.bracket,
+                "LBracket": tags.bracket,
+                "RBracket": tags.bracket,
+                })
         ]
     }),
     name: "leanVerbose"
